24c658c3476bc22c1cc9b72a509a809006b53d24
notsajeed <sajeed725333@gmail.com>
2026-03-07 17:03:42 +0530

    use upstash instead of kv

--- src/app/api/leetcode/route.ts
+++ src/app/api/leetcode/route.ts
@@ -1,4 +1,8 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+const kv = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
 import { NextRequest, NextResponse } from "next/server";
 
 const CITY_KEY = "leetcity:users";
